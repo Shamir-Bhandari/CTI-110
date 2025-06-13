e9a584aa01b4123ad30547ef652582226b7d079d
Shamir-Bhandari <bhandars1187@student.faytechcc.edu>
2025-06-12 22:06:47 -0400

    Add files via upload

---
+++ P1H1BhandariShamir.py
@@ -0,0 +1,29 @@
+#Shamir Bhandari
+#6/12/2025
+#P1H1
+#Make a Semi-working calulator.
+
+print("---------Calculating Exponenets------")
+
+
+base_value= (int(input("Enter an integer as the base value:")))
+exp_value= (int(input("Enter an integer as the exponent:")))
+
+power_value= base_value ** exp_value
+
+print(base_value, "raised to the power of", exp_value, "is", power_value, "!!")
+
+print("----------Addition and Subtraction-----")
+
+starting_integer= (int(input("Enter a starting integer:")))
+integer_add= (int(input("Enter an integer to add:")))
+interger_subtract= (int(input("Enter an integer to subtract:")))
+
+final_value= starting_integer + integer_add - interger_subtract
+
+print(starting_integer, "+", integer_add, "-", interger_subtract, "is equal to", final_value)
+
+
+
+      
+
