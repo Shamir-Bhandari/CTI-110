699cd1e257282f058658a74af11e6c4f13bc5a8e
Shamir-Bhandari <bhandars1187@student.faytechcc.edu>
2025-06-20 11:54:03 -0400

    Add files via upload

---
+++ P2LAB1BhandariShamir.py
@@ -0,0 +1,21 @@
+#Shamir Bhandari
+#6/20/25
+#P2Lab1
+#Circle math equations
+
+import math
+radius = float(input("What is the radius of the Circle: "))
+
+diameter =radius * 2
+
+circumference = 2 * math.pi * radius
+
+area = math.pi * radius ** 2
+
+print()
+print(f"The diameter of the circle is {diameter:.1f}")
+print()
+print(f"The circumference of the circle is {circumference:.2f}")
+print()
+print(f"The area of the circle is {area:.3f}")
+
