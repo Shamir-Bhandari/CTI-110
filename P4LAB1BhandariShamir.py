cf35dd68357c218a0c7cea66169831ed00a2e202
Shamir-Bhandari <bhandars1187@student.faytechcc.edu>
2025-07-04 10:29:10 -0400

    Add files via upload

---
+++ P4LAB1BhandariShamir.py
@@ -0,0 +1,18 @@
+#Shamir Bhandari
+#7/4/2025
+#P4LAB1
+#Drawing
+
+
+import turtle
+
+
+for t in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+
+for t in range(3):
+    turtle.forward(100)
+    turtle.left(120)
+
